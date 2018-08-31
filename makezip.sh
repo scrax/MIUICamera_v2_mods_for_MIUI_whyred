c64661dda38262fa8af93ab6b09662f1c0ddfe6f
scrax <alessio@MacBook-Pro-di-Alessio.local>
2018-08-31 22:30:15 +0200

    fixes makezip

--- makezip.sh
+++ makezip.sh
@@ -4,19 +4,19 @@ name=MIUICamera_v2_whyred
 ver=$(cat module.prop | grep version= | cut -b 10-13)
 
 echo ' '
-echo make ZIP: TWRP-$name-$ver_$1.zip
+echo make ZIP: TWRP-$name-$ver\_$1.zip
 echo '---------'
 cp make/twrp META-INF/com/google/android/updater-script
-zip -r -X TWRP-$name-$ver_$1.zip mount META-INF system tmp
+zip -r -X TWRP-$name-$ver\_$1.zip mount META-INF system tmp
 echo '---------'
 echo FINISHED!
 echo ' '
 
 echo ' '
-echo make ZIP: MAGISK-$name-$ver_$1.zip
+echo make ZIP: MAGISK-$name-$ver\_$1.zip
 echo '---------'
 cp make/magisk META-INF/com/google/android/updater-script
-zip -r -X MAGISK-$name-$ver_$1.zip common META-INF system config.sh module.prop README.md
+zip -r -X MAGISK-$name-$ver\_$1.zip common META-INF system config.sh module.prop README.md
 echo '---------'
 echo FINISHED!
 echo ' '
