5671a3fa2642434da05328e7d50c3621eef3898a
scrax <alessio@MacBook-Pro-di-Alessio.local>
2018-08-31 06:20:03 +0200

    script for magisk and twrp zip
    made one package for twrp and magisk module

--- makezip.sh
+++ makezip.sh
@@ -1,12 +1,21 @@
-#!/sbin/sh
+#!/bin/sh
 
 name=MIUICamera_v2_whyred
 ver=$(cat module.prop | grep version= | cut -b 10-13)
 
 echo ' '
-echo make ZIP: $name-$ver.zip
+echo make ZIP: MAGISK-$name-$ver_$1.zip
 echo '---------'
-zip -r -X $name-$ver.zip common META-INF system config.sh module.prop README.md
+cp magisk META-INF/com/google/android/updater-script
+zip -r -X MAGISK-$name-$ver_$1.zip common META-INF system config.sh module.prop README.md
+echo '---------'
+echo FINISHED!
+echo ' '
+echo ' '
+echo make ZIP: TWRP-$name-$ver_$1.zip
+echo '---------'
+cp twrp META-INF/com/google/android/updater-script
+zip -r -X TWRP-$name-$ver_$1.zip mount META-INF system tmp
 echo '---------'
 echo FINISHED!
 echo ' '
---
+++ tmp/vendor_build_prop_tweaks.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+
+bp="/vendor/build.prop"
+
+busybox mount /vendor
+busybox mount /data
+
+
+
+if [ -f /vendor/build.prop.bak ]; 
+  then
+    rm -rf $bp
+    cp $bp.bak $bp
+  else
+    cp $bp $bp.bak
+fi
+
+
+echo " " >> $bp
+
+for mod in vendor_build_prop_tweaks;
+  do
+
+    for prop in `cat /tmp/$mod`;do
+      export newprop=$(echo ${prop} | cut -d '=' -f1)
+      sed -i "/${newprop}/d" /vendor/build.prop
+      echo $prop >> /vendor/build.prop
+    done
+done
+
+#dir="/system/media/audio/ui/"
+#for name in audio_fix;
+#  do
+#    if [ -f $dir$name.bak ];
+#      then
+#        mv $dir$name.bak $dir$name
+#      else
+#        mv $dir$name $dir$name.bak
+#    fi
+#done
