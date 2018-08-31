6be448eafa2d54f73b8ed4c772057f2eeaa87731
scrax <alessio@MacBook-Pro-di-Alessio.local>
2018-08-31 06:32:37 +0200

    update makezip

--- makezip.sh
+++ makezip.sh
@@ -4,18 +4,19 @@ name=MIUICamera_v2_whyred
 ver=$(cat module.prop | grep version= | cut -b 10-13)
 
 echo ' '
-echo make ZIP: MAGISK-$name-$ver_$1.zip
+echo make ZIP: $name-$ver$1.zip
 echo '---------'
-cp magisk META-INF/com/google/android/updater-script
-zip -r -X MAGISK-$name-$ver_$1.zip common META-INF system config.sh module.prop README.md
-echo '---------'
-echo FINISHED!
-echo ' '
-echo ' '
-echo make ZIP: TWRP-$name-$ver_$1.zip
-echo '---------'
-cp twrp META-INF/com/google/android/updater-script
-zip -r -X TWRP-$name-$ver_$1.zip mount META-INF system tmp
+cp make/magisk META-INF/com/google/android/updater-script
+zip -r -X $name-$ver$1.zip common META-INF system config.sh module.prop README.md
 echo '---------'
 echo FINISHED!
 echo ' '
+echo ' '
+echo make ZIP: TWRP-$name-$ver$1.zip
+echo '---------'
+cp make/twrp META-INF/com/google/android/updater-script
+zip -r -X TWRP-$name-$ver$1.zip mount META-INF system tmp
+echo '---------'
+echo FINISHED!
+echo ' '
+cp make/magisk META-INF/com/google/android/updater-script
