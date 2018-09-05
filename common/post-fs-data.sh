63ac75afea0e623ba5278ee4a55000c54bcf5bb8
scrax <alessio@MacBook-Pro-di-Alessio.local>
2018-09-05 23:04:11 +0200

    another audio fix method
    with backup for twrp

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,15 +6,18 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 resetprop ro.vendor.audio.sdk.fluencetype none;
-resetprop camera.disable_shutter_sound.packagelist com.android.camera;
+#resetprop camera.disable_shutter_sound.packagelist com.android.camera;
 
-#dir="/system/media/audio/ui/";
-#for name in audio_fix;
-#  do
-#    if [ -f $dir$name.bak ];
-#      then
-#        mv $dir$name.bak $dir$name
-#      else
-#        mv $dir$name $dir$name.bak
-#    fi
-#done
+echo 'Starting camera sound fix'
+dir="/system/media/audio/ui/";
+for name in audio_fix;
+  do
+    if [ -f $dir$name.bak ];
+      then
+        echo 'Camera sound already fixed'
+        # mv $dir$name.bak $dir$name
+      else
+        echo 'Fixing camera sound'
+        mv $dir$name $dir$name.bak
+    fi
+done
